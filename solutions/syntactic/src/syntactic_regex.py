864392bcb34a1d86debc502fc5335500a29cd6c8
albertecos <144015396+albertecos@users.noreply.github.com>
2026-08-31 17:01:33 +0200

    up

--- solutions/syntactic/src/syntactic_regex.py
+++ solutions/syntactic/src/syntactic_regex.py
@@ -39,6 +39,7 @@ def main():
     log.debug(f"found {res}")
     rest = content[res.end(0) : -1]
 
+    #looking for assertions
     assert_or_end = re.search(r"assert|(^\s*})", rest, re.MULTILINE)
 
     if not assert_or_end:
@@ -56,6 +57,42 @@ def main():
         log.debug("No assertion")
         print("assertion error;not-found")
 
+    #looking for division with zero
+    divide_or_end = re.search(r"/|(^\s*})", rest, re.MULTILINE)
+
+    if not divide_or_end:
+        log.error("Could not find end of method or divide")
+        log.error(rest)
+        sys.exit(1)
+
+    log.debug(f"found divide {divide_or_end}")
+    divide_found = divide_or_end.group(0) == "/"
+
+    if divide_found:
+        log.debug("Found divide")
+        print("divide by zero;found-div")
+    else:
+        log.debug("No divide")
+        print("divide by zero;not-found-div")
+
+    #looking for null pointer errors
+    null_or_end = re.search(r"|null|(^\s*})", rest, re.MULTILINE)
+
+    if not null_or_end:
+        log.error("Could not find end of method or null pointer")
+        log.error(rest)
+        sys.exit(1)
+
+    log.debug(f"found null pointer {null_or_end}")
+    null_found = null_or_end.group(0) == "null"
+
+    if null_found:
+        log.debug("Found null")
+        print("null pointer;found")
+    else:
+        log.debug("No null")
+        print("null pointer;not-found")
+
     for q in jpamb.QUERIES:
-        if q != "assertion error":
+        if q != "assertion error" and q != "divide by zero" and q != "null pointer":
             print(f"{q};skip")
